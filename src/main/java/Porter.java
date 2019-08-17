b9f92f337bb774b19d9246b84e44bc334c9ee87d
Dillon <18711345569@163.com>
2019-08-17 15:04:42 +0800

    finish High Card Compare

--- src/main/java/Porter.java
+++ src/main/java/Porter.java
@@ -1,6 +1,29 @@
 package main.java;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Porter {
+    public String judgeWhoWin(String s){
+        String s1 = s.substring(0,15);
+        String s2 = s.substring(15);
+        List<String> list1 = getSortedList(s1);
+        List<String> list2 = getSortedList(s2);
+        String result = "";
+        for (int i = list1.size()-1 ; i >= 0; i--){
+            if(StringToInt(list1.get(i)) > StringToInt(list2.get(i))){
+                result = "Person1Win";
+                return result;
+            }else if(StringToInt(list1.get(i)) < StringToInt(list2.get(i))){
+                result = "Person2Win";
+                return result;
+            }else {
+                result = "Peace";
+            }
+        }
+        return result;
+    }
+
     public int getSizeResult(String s1 , String s2) {
         int s1Size = StringToInt(s1.substring(0,1));
         int s2Size = StringToInt(s2.substring(0,1));
@@ -27,9 +50,27 @@ public class Porter {
                 result = 14;
                 break;
             default:
-                result = Integer.parseInt(s);
+                result = Integer.parseInt(s.substring(0,1));
                 break;
         }
         return result;
+    }
+
+    public List<String> getSortedList(String s){
+        List<String> list = new ArrayList<>();
+        for (int i = 0 ; i < 14 ; i = i + 3 ){
+            list.add(s.substring(i,i+2));
+        }
+        String tmp="";
+        for(int i=1;i<list.size()-1;i++){
+            for (int j=0;j<list.size()-i;j++){
+                if(StringToInt(list.get(j)) > StringToInt(list.get(j+1))){
+                    tmp = list.get(j);
+                    list.set(j,list.get(j+1));
+                    list.set(j+1,tmp);
+                }
+            }
+        }
+        return list;
     }
 }
--- src/test/java/PorterTest.java
+++ src/test/java/PorterTest.java
@@ -19,4 +19,17 @@ public class PorterTest {
         assertEquals(1,result2);
         assertEquals(0,result3);
     }
+    @Test
+    public void should_return_Person2Win_when_2H_3H_5C_6D_8S_5H_6S_8S_9C_10C(){
+        //given
+        Porter porter = new Porter();
+        //when
+        String result1 = porter.judgeWhoWin("2H 3H 5C 6D 8S 5H 6S 8S 9C 10C");
+        String result2 = porter.judgeWhoWin("2H 3H 5C 6D 8S 2H 3H 5C 6D 8S");
+        String result3 = porter.judgeWhoWin("2H 3H 5C JD QS 5H 6S 8S 9C QC");
+        //then
+        assertEquals("Person2Win",result1);
+        assertEquals("Peace",result2);
+        assertEquals("Person1Win",result3);
+    }
 }
