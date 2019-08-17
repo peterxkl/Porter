a5e2b19fb0291cbf0a3fe1e8bba6d9cda62ad5c4
Dillon <18711345569@163.com>
2019-08-17 10:20:24 +0800

    finish 2C to JC test

--- src/main/java/Porter.java
+++ src/main/java/Porter.java
@@ -1,4 +1,35 @@
 package main.java;
 
 public class Porter {
+    public int getSizeResult(String s1 , String s2) {
+        int s1Size = StringToInt(s1.substring(0,1));
+        int s2Size = StringToInt(s2.substring(0,1));
+        if (s1Size > s2Size) return 1;
+        else if (s1Size < s2Size) return 2;
+        else if (s1Size == s2Size) return 0;
+        return 4;//错误返回4
+    }
+
+    public int StringToInt(String s){
+        char a = s.charAt(0);
+        int result = 0;
+        switch (a) {
+            case 'J':
+                result = 11;
+                break;
+            case 'Q':
+                result = 12;
+                break;
+            case 'K':
+                result = 13;
+                break;
+            case 'A':
+                result = 14;
+                break;
+            default:
+                result = Integer.parseInt(s);
+                break;
+        }
+        return result;
+    }
 }
--- src/test/java/PorterTest.java
+++ src/test/java/PorterTest.java
@@ -1,4 +1,22 @@
 package test.java;
 
+import main.java.Porter;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PorterTest {
+    @Test
+    public void should_return_1_when_2C_JC(){
+        //given
+        Porter porter = new Porter();
+        //when
+        int result1 = porter.getSizeResult("2C","JC");
+        int result2 = porter.getSizeResult("AC","JC");
+        int result3 = porter.getSizeResult("AC","AS");
+        //then
+        assertEquals(2,result1);
+        assertEquals(1,result2);
+        assertEquals(0,result3);
+    }
 }
